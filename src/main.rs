f68af472320bec4bb3d7c0540da6b3e405ac0066
Ryan Lynch <lynchryan2@gmail.com>
2024-03-29 23:04:48 +0000

    Removing UI, starting as command line tool.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-slint::include_modules!();
+// slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+const DEPOSITPER: f64 = 0.1;
+const SOLFEES: f64 = 2500.0;
+const INSPECTFEES: f64 = 1500.0;
+const STAMPDUTYPER: f64 = 0.01;
 
-    // ui.on_request_increase_value({
-    //     let ui_handle = ui.as_weak();
-    //     move || {
-    //         let ui = ui_handle.unwrap();
-    //         ui.set_counter(ui.get_counter() + 1);
-    //     }
-    // });
+fn main() {
+    let mut house_price_input = String::new();
+    println!("Enter the price of the house: ");
+    std::io::stdin().read_line(&mut house_price_input).unwrap();
 
-    ui.run()
+    let house_price: f64 = house_price_input.trim().parse().unwrap();
+    let deposit: f64 = house_price * DEPOSITPER;
+    // let solicitor_fees: f64 = number * SOLFEES;
+    // let inspector_fees: f64 = number * INSPECTFEES;
+    let stamp_duty: f64 = house_price * STAMPDUTYPER;
+    let savings_required = deposit + SOLFEES + INSPECTFEES + stamp_duty;
+    let result = format!("Deposit:\t\t €{:.2}\nSolicitor Fees:\t\t €{:.2}\nInspection Fees:\t €{:.2}\nStamp Duty:\t\t €{:.2}\n\nRequired Savings:\t €{:.2}", deposit, SOLFEES, INSPECTFEES, stamp_duty, savings_required);
+    println!("{}", result);
 }
