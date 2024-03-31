15b985b68f91b8d40c59d2abfc9c58aa9b4af061
Ryan Lynch <ryanlynch@microsoft.com>
2024-03-31 14:34:27 +0100

    Adding loop and user input error handling.

--- src/calculate_savings_for_house.rs
+++ src/calculate_savings_for_house.rs
@@ -10,29 +10,48 @@ const STAMPDUTYPER: f64 = 0.01;
  * Calculate the savings required for a house.
  */
 pub fn calculate_savings_for_house() {
-    println!("\nEnter the price of the house: €");
+    loop {
+        println!("\nEnter the price of the house: €");
 
-    let mut house_price = String::new();
+        let mut house_price = String::new();
 
-    // Read the input from the user
-    io::stdin()
-        .read_line(&mut house_price)
-        .expect("Failed to read line");
+        // Read the input from the user
+        io::stdin()
+            .read_line(&mut house_price)
+            .expect("Failed to read line");
 
-    // Convert the input to a number
-    let house_price: f64 = house_price
-        .trim()
-        .parse()
-        .expect("Please enter a valid number");
+        // Convert the input to a number
+        let house_price: f64 = match house_price.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
+        };
 
-    // Calculate the required savings
-    let deposit: f64 = house_price * DEPOSITPER;
-    let stamp_duty: f64 = house_price * STAMPDUTYPER;
-    let savings_required = calculate_total_fees(deposit, stamp_duty);
+        // Calculate the required savings
+        let deposit: f64 = house_price * DEPOSITPER;
+        let stamp_duty: f64 = house_price * STAMPDUTYPER;
+        let savings_required = calculate_total_fees(deposit, stamp_duty);
 
-    // Print the result
-    let result = format!("\nDeposit:\t\t €{:.2}\nSolicitor Fees:\t\t €{:.2}\nEvaluation Fees:\t €{:.2}\nSurveyor Fees:\t\t €{:.2}\nStamp Duty:\t\t €{:.2}\n\nRequired Savings:\t €{:.2}", deposit, SOLFEES, EVALFEES, SURVEYFEES, stamp_duty, savings_required);
-    println!("{}\n", result);
+        // Print the result
+        let result = format!("\nDeposit:\t\t €{:.2}\nSolicitor Fees:\t\t €{:.2}\nEvaluation Fees:\t €{:.2}\nSurveyor Fees:\t\t €{:.2}\nStamp Duty:\t\t €{:.2}\n\nRequired Savings:\t €{:.2}", deposit, SOLFEES, EVALFEES, SURVEYFEES, stamp_duty, savings_required);
+        println!("{}\n", result);
+
+        // Ask the user if they want to calculate again
+        println!("Do you want to calculate the savings for another house? (y/n)");
+
+        let mut answer = String::new();
+
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
+
+        if answer.trim() != "y" {
+            println!("\nGoodbye! Best of luck with your house purchase!\n");
+            break;
+        }
+    }
 }
 
 /**
