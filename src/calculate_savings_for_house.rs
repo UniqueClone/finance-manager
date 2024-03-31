f3816562630a082059815072bab084ae1ff35b0a
Ryan Lynch <ryanlynch@microsoft.com>
2024-03-31 14:21:33 +0100

    Adding comments and renaming calcualte_required_savings to calculate_total_fees

--- src/calculate_savings_for_house.rs
+++ src/calculate_savings_for_house.rs
@@ -6,31 +6,39 @@ const EVALFEES: f64 = 150.0;
 const SURVEYFEES: f64 = 500.0;
 const STAMPDUTYPER: f64 = 0.01;
 
+/**
+ * Calculate the savings required for a house.
+ */
 pub fn calculate_savings_for_house() {
     println!("\nEnter the price of the house: €");
 
     let mut house_price = String::new();
 
+    // Read the input from the user
     io::stdin()
         .read_line(&mut house_price)
         .expect("Failed to read line");
 
+    // Convert the input to a number
     let house_price: f64 = house_price
         .trim()
         .parse()
         .expect("Please enter a valid number");
 
+    // Calculate the required savings
     let deposit: f64 = house_price * DEPOSITPER;
     let stamp_duty: f64 = house_price * STAMPDUTYPER;
-    let savings_required = calculate_required_savings(house_price);
+    let savings_required = calculate_total_fees(deposit, stamp_duty);
 
+    // Print the result
     let result = format!("\nDeposit:\t\t €{:.2}\nSolicitor Fees:\t\t €{:.2}\nEvaluation Fees:\t €{:.2}\nSurveyor Fees:\t\t €{:.2}\nStamp Duty:\t\t €{:.2}\n\nRequired Savings:\t €{:.2}", deposit, SOLFEES, EVALFEES, SURVEYFEES, stamp_duty, savings_required);
     println!("{}\n", result);
 }
 
-fn calculate_required_savings(house_price: f64) -> f64 {
-    let deposit: f64 = house_price * DEPOSITPER;
-    let stamp_duty: f64 = house_price * STAMPDUTYPER;
+/**
+ * Calculate the total fees required for a house given the deposit and stamp duty.
+ */
+fn calculate_total_fees(deposit: f64, stamp_duty: f64) -> f64 {
     let savings_required = deposit + SOLFEES + EVALFEES + SURVEYFEES + stamp_duty;
 
     return savings_required;
